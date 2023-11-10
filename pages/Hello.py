07aa41dde4767c3977bbb68650cb106072728065
Sully Sullenberger <sully@sadburger.com>
2023-11-10 11:15:07 -0800

    indent

--- pages/Hello.py
+++ pages/Hello.py
@@ -5,4 +5,4 @@ from app_setup import set_page_config
 set_page_config("Hello")
 from misc_shared.models import Example  # noqa: E402
 
-st.code(Example.create_new({"name": "Test"}).model_dump_json())
+st.code(Example.create_new({"name": "Test"}).model_dump_json(indent=2))
