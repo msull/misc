39cead57893243a4cb7b016311f823e1f0ccddd6
Sully Sullenberger <sully@sadburger.com>
2023-11-10 11:12:34 -0800

    Tweak example page

--- pages/Hello.py
+++ pages/Hello.py
@@ -5,4 +5,4 @@ from app_setup import set_page_config
 set_page_config("Hello")
 from misc_shared.models import Example  # noqa: E402
 
-st.write(Example.create_new({"name": "Test"}))
+st.code(Example.create_new({"name": "Test"}).model_dump_json())
