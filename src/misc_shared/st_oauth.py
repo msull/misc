937e0b5d8a2b34763560482e480cab6604cccb60
Sully Sullenberger <sully@sadburger.com>
2023-12-11 11:23:40 -0800

    try to stop event propagation

--- src/misc_shared/st_oauth.py
+++ src/misc_shared/st_oauth.py
@@ -58,7 +58,7 @@ def show_auth_link(config, label):
     # if st.experimental_get_query_params():
     #     qpcache = qparms_cache(state_parameter)
     #     qpcache = st.experimental_get_query_params()
-    st.markdown(f'<a href="{request_url}" target="_self">{label}</a>', unsafe_allow_html=True)
+    st.markdown(f'<a href="{request_url}" onclick="event.stopPropagation();" target="_self">{label}</a>', unsafe_allow_html=True)
     st.stop()
 
 
